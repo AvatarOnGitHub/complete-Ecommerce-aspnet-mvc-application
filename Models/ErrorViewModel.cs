bc24a6cb95566a191111ccdf333818fb45790dcc
Daniel <Daniel@ESMERALDA>
2023-01-15 22:46:54 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CinemaPlovdiv.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
